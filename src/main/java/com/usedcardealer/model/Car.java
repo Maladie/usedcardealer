04f80d0ac7d17f8d0572289c2a855a4a05c4ea33
bkomoder <bartoszkomoder@gmail.com>
2017-10-27 18:38:22 +0200

    added relation between Car and Dealer

--- src/main/java/com/usedcardealer/model/Car.java
+++ src/main/java/com/usedcardealer/model/Car.java
@@ -1,9 +1,6 @@
 package com.usedcardealer.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -22,6 +19,8 @@ public class Car {
     private String type;
     @NotNull
     private boolean isReserved;
+    @ManyToOne
+    private Dealer dealer;
 
 
 }
