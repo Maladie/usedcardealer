091e0b536bf955c75924ded9794940c05e18c386
krzychu124 <krzychu124@gmail.com>
2017-10-27 18:42:20 +0200

    car, dealer repos

--- src/main/java/com/usedcardealer/dao/DealerDao.java
+++ src/main/java/com/usedcardealer/dao/DealerDao.java
@@ -3,7 +3,12 @@ package com.usedcardealer.dao;
 
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 @Repository
 public class DealerDao {
-
+    @PersistenceContext
+    private EntityManager entityManager;
 }
