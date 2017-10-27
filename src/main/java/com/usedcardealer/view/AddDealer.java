a892c2925786dab68d4aa889f8ef13cc2d974975
krzychu124 <krzychu124@gmail.com>
2017-10-27 20:26:59 +0200

    addDealer class

---
+++ src/main/java/com/usedcardealer/view/AddDealer.java
@@ -0,0 +1,43 @@
+package com.usedcardealer.view;
+
+public class AddDealer {
+
+    private String name;
+    //address
+    private String street;
+    private String city;
+    private String country;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
