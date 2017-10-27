dc904d5326927a0b6220a735fbce106724664308
krzychu124 <krzychu124@gmail.com>
2017-10-27 19:17:48 +0200

    autowired

--- src/main/java/com/usedcardealer/services/CarService.java
+++ src/main/java/com/usedcardealer/services/CarService.java
@@ -1,6 +1,7 @@
 package com.usedcardealer.services;
 
 import com.usedcardealer.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class CarService {
 
     private CarRepository carRepository;
 
+    @Autowired
     public CarService(CarRepository carRepository){
         this.carRepository = carRepository;
     }
