ab5b090b1ca9576334fe1063556697acbb7c213a
krzychu124 <krzychu124@gmail.com>
2017-10-28 11:48:34 +0200

    getCarById

--- src/main/java/com/usedcardealer/repository/CarRepository.java
+++ src/main/java/com/usedcardealer/repository/CarRepository.java
@@ -12,4 +12,5 @@ public interface CarRepository extends JpaRepository<Car, Integer> {
     List<Car> findByCompany(String companyName);
     List<Car> findByCompanyAndModel(String companyName, String modelName);
     Car findByIdAndIsReserved(Integer id, boolean isReserved);
+    Car findById(Integer id);
 }
